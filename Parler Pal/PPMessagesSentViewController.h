8769471ae5f91f4b03ffb83d7a7d29167afe33bd
Aaron Vizzini <avizzv92@gmail.com>
2014-06-29 01:22:43 -0400

    This view controller displays all the messages that have been sent by the user.
    This view controller displays all the messages that have been sent by the user.
    
    Signed-off-by: Aaron Vizzini <avizzv92@gmail.com>

---
+++ Parler Pal/PPMessagesSentViewController.h
@@ -0,0 +1,25 @@
+//
+//  PPMessagesSentViewController.h
+//  Parler Pal
+//
+//  Created by Aaron Vizzini on 6/28/14.
+//  Copyright (c) 2014 AaronVizzini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PPMessagePopupView.h"
+#import "PPDatabaseManager.h"
+#import "PPDataShare.h"
+
+@interface PPMessagesSentViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+{
+    IBOutlet UIBarButtonItem *sidebarButton;
+    IBOutlet UITableView *table;
+    NSMutableArray *messages;
+    PPMessagePopupView *messageContentView;
+}
+@property(nonatomic, strong) IBOutlet UIBarButtonItem *sidebarButton;
+@property(nonatomic, strong) IBOutlet UITableView *table;
+
+@end
