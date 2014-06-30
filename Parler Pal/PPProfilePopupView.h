51dbe064f66007e3a6c0b12adbcf66b7a47016aa
Aaron Vizzini <avizzv92@gmail.com>
2014-06-30 01:53:16 -0400

    New profile pop up for displaying all of a friends public information. Languages, contact, profile, etc...
    New profile pop up for displaying all of a friends public information. Languages, contact, profile, etc...
    
    Signed-off-by: Aaron Vizzini <avizzv92@gmail.com>

---
+++ Parler Pal/PPProfilePopupView.h
@@ -0,0 +1,17 @@
+//
+//  PPProfilePopupView.h
+//  Parler Pal
+//
+//  Created by Aaron Vizzini on 6/29/14.
+//  Copyright (c) 2014 AaronVizzini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPProfilePopupView : UIView
+@property(nonatomic, weak) IBOutlet UILabel *username, *country, *email, *skype, *age, *gender;
+@property(nonatomic, weak) IBOutlet UITextView *profile, *languages;
+@property(nonatomic, weak) IBOutlet UIView *view;
+@property(nonatomic, weak) id delegate;
+-(void)show;
+@end
---
+++ Parler Pal/PPProfilePopupView.m
@@ -0,0 +1,71 @@
+//
+//  PPProfilePopupView.m
+//  Parler Pal
+//
+//  Created by Aaron Vizzini on 6/29/14.
+//  Copyright (c) 2014 AaronVizzini. All rights reserved.
+//
+
+#import "PPProfilePopupView.h"
+#import "PPDatabaseManager.h"
+
+@implementation PPProfilePopupView
+@synthesize username, country, email, skype, age, gender, profile, languages, view, delegate;
+#pragma mark -
+#pragma mark init methods
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+-(id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+#pragma mark -
+#pragma mark setup methods
+
+-(void)setup
+{
+    self.layer.masksToBounds = NO;
+    self.layer.cornerRadius = 8;
+    self.layer.shadowOffset = CGSizeMake(-4, 4);
+    self.layer.shadowRadius = 2;
+    self.layer.shadowOpacity = 0.5;
+    
+    [[NSBundle mainBundle]loadNibNamed:@"PPProfilePopupView" owner:self options:nil];
+    [self addSubview:self.view];
+    [self setTransform:CGAffineTransformMakeScale(0, 0)];
+}
+
+#pragma mark -
+#pragma mark action methods
+
+-(IBAction)hide:(id)sender
+{
+    [UIView animateWithDuration:.3 delay:0.0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        [self setTransform:CGAffineTransformMakeScale(0.0, 0.0)];
+    } completion:^(BOOL finished){[self removeFromSuperview];}];
+}
+
+#pragma mark -
+#pragma mark visibility methods methods
+
+-(void)show
+{
+    [UIView animateWithDuration:.3 delay:0.0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        [self setTransform:CGAffineTransformMakeScale(1.0, 1.0)];
+    } completion:^(BOOL finished){}];
+}
+
+@end
